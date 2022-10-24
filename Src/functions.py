74472205b421570149ff996dd50d679dc5ccdafc
Guillermo Nespral <gnsg32@gmail.com>
2022-10-24 13:55:54 +0200

    Uploaded project

--- Src/functions.py
+++ Src/functions.py
@@ -6,9 +6,6 @@ import json
 import sys
 import re
 
-
-
-
 # Function for extracting values from the chaos column.
 
 # This one is just for the Award Year
@@ -34,7 +31,7 @@ def rename_columns(column, newname):
   
 # Dropping columns
 def drop_column(datfr, column):
-    datfr = datfr.drop([column], axis = 1)
+    datfr = datfr.drop([column], axis = 1, inplace= True)
     return
 
 # Calculating the age of each Nobel awarded
@@ -55,9 +52,50 @@ def drop_nan(df):
     dfr.dropna(axis=0, inplace=True, thresh=6)
     df1.reset_index(drop=True, inplace=True)
     return
+
 # This function is used for sort and reset the df.
 def sort_reset(dfr):
     dfr.sort_values(by = ["AwardYear"], inplace = True)
     dfr.reset_index(drop=True, inplace=True)
     return
     
+
+# Deleting empty elements in a list from webscrapping   
+def compact(lst):
+    return list(filter(None, lst))
+
+# -----------------------------------------------------------------
+
+
+# Cleaning the data from webscrapping
+
+def compact(lst):
+    return list(filter(None, lst))
+
+#Regex on the columns for the GDP
+
+def n_column(df_, column1, columnn):  
+    pattern = "\d{1,3}[\,\.]\d{1,3}[\,\.]\d{1,3}|\d{1,3}[\,\.]\d{1,3}"
+    df_[columnn] = df_[column1].apply(lambda x: re.findall(pattern, str(x)))
+    return
+
+#------------------------------------------------------------------
+#Cleaning the PISA report
+
+def delete_rows(column, value):
+    df3.drop(df3.loc[df3[column]==value].index, inplace=True)
+    return  
+
+# Deleting the rows by countries we are not interested in
+
+def delete_rows_country(column):
+    for i in Countries:
+        if i not in column:
+            df3.drop(df3.loc[df3[column]==i].index, inplace=True)
+    df3.reset_index(drop=True, inplace=True)
+    return
+         
+Countries= ['Albania', 'Algeria', 'Argentina', 'Armenia', 'Azerbaijan', 'Bahrain', 'Belize', 'Benin', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cameroon', 'Chad', 'Chile', 'Colombia', 'Comoros', 'Congo, Rep.', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Dominican Republic', 'Ecuador', 'Egypt, Arab Rep.', 'El Salvador', 'Ethiopia', 'Gabon', 'Georgia', 'Ghana', 'Greece', 'Guatemala', 'Guyana', 'Honduras', 'Hungary', 'Indonesia', 'Iran, Islamic Rep.', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Latvia', 'Lebanon', 'Lesotho', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao SAR, China', 'Macedonia, FYR', 'Madagascar', 'Malawi', 'Malaysia', 'Mali', 'Malta', 'Mauritius', 'Moldova', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Namibia', 'Nicaragua', 'Niger', 'Oman', 'Panama', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Qatar', 'Rwanda', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Slovak Republic', 'Slovenia', 'South Africa', 'Swaziland', 'Syrian Arab Republic', 'Tanzania', 'Thailand', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Uganda', 'Ukraine', 'United Arab Emirates', 'Venezuela, RB', 'Vietnam', 'Yemen, Rep.', 'Zambia', 'Zimbabwe']
+
+lis = ['Australia','Belgium','Brazil','Canada','Denmark','Estonia','Finland', 'France','Germany','Greece','Ireland','Italy', 'Japan','Korea, Rep.''Mexico','Morocco','Netherlands' ,'New Zealand', 'Norway', 'Portugal', 'Russian Federation','South Africa',  'Spain', 'Sweden', 'Switzerland', 'United Kingdom', 'United States']     
+
